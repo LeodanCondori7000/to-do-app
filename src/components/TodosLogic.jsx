05198f3a0c68e963b892c5682c297876219a53bd
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-06-20 16:39:28 -0500

    first commit

---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import InputTodo from "@/components/InputTodo";
+import TodosList from "@/components/TodosList";
+
+const TodosLogic = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: uuidv4(),
+      title: "Setup development environment",
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      title: "Develop website and add content",
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      title: "Deploy to live server",
+      completed: false,
+    },
+  ]);
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <InputTodo addTodoItem={addTodoItem} />
+      <TodosList
+        todosProps={todos}
+        setTodos={setTodos}
+        delTodo={delTodo}
+        setUpdate={setUpdate}
+      />
+    </div>
+  );
+};
+
+export default TodosLogic;
